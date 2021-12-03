142f0342cf891dc2218f617b415c4d9513bcb64c
Jose-Puente2001 <josegabrielpuente75@gmail.com>
2021-12-03 11:11:21 -0400

    first commit

--- App.js
+++ App.js
@@ -2,36 +2,16 @@ import { StatusBar } from 'expo-status-bar';
 import React, {useState} from 'react';
 import { StyleSheet, Button, Text, View } from 'react-native';
 import Editor from "@monaco-editor/react";
-import file from "./components/files";
 
 export default function App() {
 
-  const [filename, setFilename] = useState('app.js');
-  const files = file[filename];
-
   return (
     <View>
-      <Button
-       title="app.js"
-       disabled={filename === "app.js"}
-       onPress={() => setFilename("app.js")}
-      />
-      <Button
-       title="style.css"
-       disabled={filename === "style.css"}
-       onPress={() => setFilename("style.css")}
-      />
-      <Button
-       title="index.html"
-       disabled={filename === "index.html"}
-       onPress={() => setFilename("index.html")}
-      />
       <Editor
           height="90vh"
           theme="vs-dark"
-          defaultLanguage={file.language}
-          path={file.name}
-          defaultValue={file.value}
+          defaultLanguage="javascript"
+          defaultValue="//Code Javacript"
       />
     </View>
   );
